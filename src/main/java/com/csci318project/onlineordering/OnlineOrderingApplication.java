2315a596cf7e4ec4db24d0309e0ee789b23b9f24
Mark Lee <mlcy0508@gmail.com>
2021-08-07 22:11:37 +1000

    Initial Commit
    Wrote the basic frame of some of the classes. However, I am unsure of what form of identification we should use to ID a Customer for lookup and updating.
    
    Do tell if something that I have wrote is completely wrong or does not make sense. I learned this watching Youtube.

---
+++ src/main/java/com/csci318project/onlineordering/OnlineOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.csci318project.onlineordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineOrderingApplication.class, args);
+	}
+
+}
