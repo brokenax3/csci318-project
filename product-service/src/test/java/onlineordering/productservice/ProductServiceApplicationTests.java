878a894a00b9a08eab30df69b4ca61d002d2cc82
Mark Lee <mlcy0508@gmail.com>
2021-09-18 20:06:29 +1000

    Create directories for customer, ordering and product service

---
+++ product-service/src/test/java/onlineordering/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package onlineordering.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
